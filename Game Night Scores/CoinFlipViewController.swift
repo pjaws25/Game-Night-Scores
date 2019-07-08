da958ba598b007b99a8dd5c71c71968b6b3f6471
Peter Jaworski <pjaws>
2019-07-07 21:18:19 -0400

    Added animation to coin flip with new images

--- Game Night Scores/CoinFlipViewController.swift
+++ Game Night Scores/CoinFlipViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class CoinFlipViewController: UIViewController {
     
     @IBOutlet weak var coinImage: UIImageView!
+    @IBOutlet weak var coinLabel: UILabel!
     
-    let coinArray = ["quarter-png-siede-7", "us-quarter-png"]
+    var coinArray: [UIImage] = []
+    //let coinArray = ["quarter-png-siede-7", "us-quarter-png"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createCoinImagesArray(count: 26, name: "coin")
 
         // Do any additional setup after loading the view.
     }
@@ -23,15 +26,40 @@ class CoinFlipViewController: UIViewController {
 
     @IBAction func flipButtonPressed(_ sender: Any) {
         
-        coinImage.image = nil
-        //generate random number
-       let randomNumber = Int(arc4random_uniform(2))
+        flipAnimation(imageView: coinImage, images: coinArray)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.05) {
+            self.flipCoin()
+        }
         
-        //random number should decide which index in array.
+//        coinImage.image = nil
+//        //generate random number
+//       let randomNumber = Int(arc4random_uniform(2))
+//
+//        //random number should decide which index in array.
+//
+//        //update the coinImage based on the random index number
+//        coinImage.image = UIImage(named: coinArray[randomNumber])
         
-        //update the coinImage based on the random index number
-        coinImage.image = UIImage(named: coinArray[randomNumber])
-        
+    }
+    func flipCoin(){
+        let coins = [1,2]
+        if let coin = coins.randomElement() {
+            coin == 1 ? (coinLabel.text = "Heads") : (coinLabel.text = "Tails")
+            coin == 1 ? (coinImage.image = UIImage(named: "Heads")): (coinImage.image = UIImage(named: "Tails"))
+        }
+    }
+    func createCoinImagesArray(count: Int, name: String){
+        for i in 1...count {
+            let imageName = UIImage(named: "\(name)\(i)")
+            coinArray.append(imageName!)
+        }
+    }
+    
+    func flipAnimation(imageView: UIImageView, images: [UIImage]){
+        imageView.animationImages = images
+        imageView.animationDuration = 1.0
+        imageView.animationRepeatCount = 1
+        imageView.startAnimating()
     }
     /*
     // MARK: - Navigation
